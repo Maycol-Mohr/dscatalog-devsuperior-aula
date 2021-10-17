5575fced5e10139cf4f8ee7730e86dbd1ed26a0c
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-10-17 14:36:37 +0200

    Fix: Postgres compatibility

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscatalog.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.devsuperior.dscatalog.entities.User;
@@ -8,4 +10,6 @@ import com.devsuperior.dscatalog.entities.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	User findByEmail(String email);
+	
+	List<User> findByFirstName(String firstName);
 }
