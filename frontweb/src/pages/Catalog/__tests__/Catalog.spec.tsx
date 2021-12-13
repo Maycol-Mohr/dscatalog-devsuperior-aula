268740325fae3170cd482bb2a5691d1e490e0f2d
Maycol Mohr <maycolmohr@yahoo.com.br>
2021-12-13 12:39:23 +0100

    Catalog, waitFor, async-await

---
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import Catalog from "..";
+import history from "util/history";
+
+test('should render Catalog with products', async () => {
+    //ARRANGE
+
+    //ACT
+    render(
+        <Router history={history}>
+          <Catalog />
+        </Router>
+    );
+
+    //ASSERT
+    expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
+
+    await waitFor(() => {
+        expect(screen.getByText('Smart TV')).toBeInTheDocument();    
+    });    
+ });
